1f814dc7547c59137064af6c588c768677bd48cb
amalvar18 <lamavar17svc@outlook.com>
2018-07-02 22:53:19 +0530

    added gradient descent

---
+++ coursework/naive_bayes_svm_classifiers/svm_demo.py
@@ -0,0 +1,52 @@
+import sys
+#sys.path.append("D:/Dev/GitRepos/ud120-projects/choose_your_own/")
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+########################## SVM #################################
+### we handle the import statement and SVC creation for you here
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=0.3, gamma=1.0)
+
+#### now your job is to fit the classifier
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+clf.fit(features_train, labels_train)
+
+#### store your predictions in a list named pred
+pred = clf.predict(features_test)
+
+### the training data (features_train, labels_train) have both "fast" and "slow"
+### points mixed together--separate them so we can give them different colors
+### in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+################################################################################
+
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
